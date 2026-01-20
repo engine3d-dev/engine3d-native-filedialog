1947a067028573b91f05713b5189e666d0f3cb3c
SpinnerX <heraaronhotmail@yahoo.com>
2026-01-19 16:21:49 -0800

    Changed version from 2.0 to 3.0

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.files import copy
 
 class NativeFileDialog(ConanFile):
     name = "nfd"
-    version = "2.0"
+    version = "3.0"
     package_type = "library"
     license = "Appache-2.0"
     homepage = "https://github.com/engine3d-dev/nfd"
